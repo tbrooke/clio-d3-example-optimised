f9ff90d3180191e03b419e17e865c262640811fa
dinob0t <dean.hillan@gmail.com>
2014-02-09 11:04:05 +1100

    Edit fonts and transition bounce

--- server.rb
+++ server.rb
@@ -44,39 +44,41 @@ module Example
 
         languages = []
         language_obj.each do |lang, count|
-          languages.push :language => lang, :count => count
+          languages.push :name => lang, :count => count
         end
         
-        language_byte_count = []
-        repos.each do |repo|
-          repo_name = repo.name
-          repo_langs = [];
-          begin
-            repo_url = "#{github_user.login}/#{repo_name}"
-            puts repo_url
-            repo_langs = octokit_client.languages(repo_url)
-          rescue Octokit::NotFound
-              puts "Error retrieving languages for #{repo_url}"
-          end
-          if !repo_langs.empty?  
-            repo_langs.each do |lang, count|
-              if !language_obj[lang]
-                language_obj[lang] = count
-              else
-                language_obj[lang] += count
-              end
-            end
-          end
-        end
+        # language_byte_count = []
+        # repos.each do |repo|
+        #   repo_name = repo.name
+        #   repo_langs = [];
+        #   begin
+        #     repo_url = "#{github_user.login}/#{repo_name}"
+        #     puts repo_url
+        #     repo_langs = octokit_client.languages(repo_url)
+        #   rescue Octokit::NotFound
+        #       puts "Error retrieving languages for #{repo_url}"
+        #   end
+        #   if !repo_langs.empty?  
+        #     repo_langs.each do |lang, count|
+        #       if !language_obj[lang]
+        #         language_obj[lang] = count
+        #       else
+        #         language_obj[lang] += count
+        #       end
+        #     end
+        #   end
+        # end
 
-        language_obj.each do |lang, count|
-          language_byte_count.push :name => "#{lang} (#{count})", :count => count
-        end
+        # language_obj.each do |lang, count|
+        #   language_byte_count.push :name => "#{lang} (#{count})", :count => count
+        # end
 
         # some mandatory formatting for d3
-        language_bytes = [ :name => "language_bytes", :elements => language_byte_count]
-
-        erb :lang_freq, :locals => { :languages => languages.to_json, :language_byte_count => language_bytes.to_json}
+        # language_bytes = [ :name => "language_bytes", :elements => language_byte_count]
+        # puts language_bytes
+        puts languages
+        erb :lang_freq, :locals => { :languages => languages.to_json}
+        # erb :lang_freq, :locals => { :languages => languages.to_json, :language_byte_count => language_bytes.to_json}
       end
     end
   end
